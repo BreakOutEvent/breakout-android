671150cb25d2eaff9333c1bc41fcfd54169eabdb
Felix Kienhöfer <flix2342@gmail.com>
2018-03-10 16:37:49 +0100

    Large Part of the new Post screen done

--- app/screens/NewPost.js
+++ app/screens/NewPost.js
@@ -10,7 +10,8 @@ import {
 	ActivityIndicator,
 	TouchableOpacity,
   Image,
-  ImageBackground
+  ImageBackground,
+  TextInput
 } from 'react-native';
 import GenericButton from '../components/GenericButton';
 
@@ -41,17 +42,37 @@ export default class NewPost extends Component{
             </TouchableOpacity>
             <View style={{flex:1}}/>
             <View style={{width:'100%', backgroundColor:'#00000080', justifyContent:'space-between'}}>
-              <Text style={{ color:'#ffffff'}}>
+              <Text style={{ marginLeft:20, color:'#ffffff'}}>
                 Aktuellen Ort ermitteln...
               </Text>
             </View>
           </ImageBackground>
+          <View style={styles.challengeSelector}>
+            <TouchableOpacity style={{justifyContent:'center'}}>
+              <Image source={require('../assets/ic_add_circle_outline_black_24dp.png')} style={{height:26, width:26, marginHorizontal:20, resizeMode: 'contain', tintColor:'#33333333'}}/>
+            </TouchableOpacity>
+            <Text style={{color:'#33333333', fontSize:15, flex:1, height:'100%', textAlignVertical:'center'}}>
+              Wähle eine Challenge aus
+            </Text>
+          </View>
+          <View style={{flex:1, backgroundColor:'#f2f2f2'}}>
+            <Text style={{color:'#00000061', fontSize:12, marginHorizontal:16, marginTop:16}}>
+              Nachricht
+            </Text>
+            <TextInput style={{color:'#000000', height:40, marginHorizontal:16}} placeholderTextColor='#00000061' underlineColorAndroid='#00000061'
+           placeholder='Was machst du gerade?'/>
+          </View>
         </View>
       );
   }
 }
 
 const styles = StyleSheet.create({
+  challengeSelector:{
+    flexDirection:'row',
+    height:70,
+    backgroundColor:'#ffffff'
+  },
   container:{
     flex:1,
     width:'100%',
